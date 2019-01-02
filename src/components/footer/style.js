17f8598b3b2e6b9bb89a54e17b51c659082419b7
liwenhao555 <719256863@qq.com>
2019-01-02 16:09:23 +0800

    【opt】改变底部颜色

--- src/components/footer/style.js
+++ src/components/footer/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import {Link} from "react-router-dom";
 
 export const Footers = styled.div`
-background: pink;
+background: red;
 position: fixed;
 bottom: 0;
 width: 375px;
